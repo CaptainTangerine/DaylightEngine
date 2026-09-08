d7225bd96bbb238327d2f00126ffa41f63358fd4
cptmandarine <wnsqhd55555@gmail.com>
2026-09-08 23:29:08 +0900

    chore: DX11에서 Vulkan으로 전환하기 위한 프로젝트 초기화

--- Daylight/Source/Graphics/GfxCommon.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <d3d11.h>
-#include <dxgi.h>
-#include <wrl/client.h>
-#include <d3dcompiler.h>
-
-template<typename T>
-using ComPtr = Microsoft::WRL::ComPtr<T>;
--- Daylight/Source/pch.h
+++ Daylight/Source/pch.h
@@ -4,7 +4,5 @@
 #include <functional>
 #include <memory>
 #include <string>
-#include <Windows.h>
 #include <utility>
 #include <filesystem>
-#include "Core/Types.h"
