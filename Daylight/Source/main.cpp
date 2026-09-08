5c6d0baa84ff24e0e868427046e217ca9f9e33ee
cptmandarine <wnsqhd55555@gmail.com>
2026-09-08 23:29:08 +0900

    프로젝트 초기화
    DX11 -> Vulkan

--- Daylight/Source/main.cpp
+++ Daylight/Source/main.cpp
@@ -1,53 +1,6 @@
 #include "pch.h"
-#include "Platform/Window.h"
-#include "Graphics/GfxDevice.h"
-#include "Graphics/GfxShader.h"
-#include "Renderer/Renderer.h"
 
-int APIENTRY wWinMain(
-    _In_     HINSTANCE hInstance,
-    _In_opt_ HINSTANCE hPrevInstance,
-    _In_     LPWSTR    lpCmdLine,
-    _In_     int       nCmdShow)
+int main()
 {
-    Window window(hInstance, L"Daylight Engine", 1280, 720);
-
-    GfxDevice gfx;
-    GfxDeviceDesc desc;
-    desc.windowHandle = window.GetHandle();
-    desc.width  = 1280;
-    desc.height = 720;
-
-#if defined(_DEBUG)
-    desc.debugLayer = true;
-#endif
-
-    if (!gfx.Init(desc))
-    {
-        return -1;
-    }
-    Renderer  renderer;
-    if (!renderer.Init(gfx))
-    {
-        return -1;
-    }
-
-    DelegateHandle resizeHandle = window.onResize.Add(
-        [&gfx](int w, int h)
-        {
-            gfx.Resize(w, h);
-        }
-    );
-
-    while (window.ProcessMessages())
-    {
-        gfx.BeginFrame();
-        renderer.Render(gfx);
-        gfx.EndFrame(true);
-    }
-
-    window.onResize.Remove(resizeHandle);
-    gfx.Shutdown();
-
     return 0;
 }
